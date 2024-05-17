82ece181a47db656d52e2519ffa8e3b2e7e5c5b0
HappyPr0gramming <156795708+HappyPr0gramming@users.noreply.github.com>
2024-05-17 14:38:06 +0200

    Add files via upload

---
+++ javascript.py
@@ -0,0 +1,23 @@
+import math
+
+class console():
+    def log(a):
+        print(a)
+
+class Math():
+    def cos(a):
+        print(math.cos(a))
+    def acos(a):
+        print(math.acos(a))
+    def asin(a):
+        print(math.asin(a))
+    def acosh(a):
+        print(math.acosh(a))
+    def asinh(a):
+        print(math.asinh(a))
+    def log(a):
+        print(math.log(a))
+    def sqrt(a):
+        print(math.sqrt(a))
+    def atan2(a):
+        print(math.atan2(a))
